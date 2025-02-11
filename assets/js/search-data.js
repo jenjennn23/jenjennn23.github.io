2da69eebb9ae3d503e85d055d3b792d908c1c994
jenjennn23 <jenniferpark323@gmail.com>
2025-02-11 17:43:21 +0000

    Deploying to gh-pages from @ jenjennn23/jenjennn23.github.io@16f5eb07ff378d6d77ae3c059702235998910119 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -452,13 +452,6 @@ ninja.data = [{
           window.open("mailto:%6A%65%6E%6E%69%66%65%72%70%61%72%6B%33%32%33@%67%6D%61%69%6C.%63%6F%6D", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
-        },
-      },{
         id: 'social-linkedin',
         title: 'LinkedIn',
         section: 'Socials',
